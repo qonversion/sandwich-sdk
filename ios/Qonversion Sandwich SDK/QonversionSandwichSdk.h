c2ed5610a2d1610df534a7faf5789e9b55f7340f
kamospertsyan <spertsiankamo@gmail.com>
2022-04-06 11:08:48 +0300

    Renaming.

---
+++ ios/Qonversion Sandwich SDK/QonversionSandwichSdk.h
@@ -0,0 +1,18 @@
+//
+//  QonversionSandwichSdk.h
+//  Qonversion Sandwich SDK
+//
+//  Created by Kamo Spertsyan on 05.04.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Qonversion_Sandwich_SDK.
+FOUNDATION_EXPORT double Qonversion_Sandwich_SDKVersionNumber;
+
+//! Project version string for Qonversion_Sandwich_SDK.
+FOUNDATION_EXPORT const unsigned char Qonversion_Sandwich_SDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Qonversion_Sandwich_SDK/PublicHeader.h>
+
+
