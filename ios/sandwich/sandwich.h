2d6e4044b00bc9969a2c6e827091ac64065fc792
Kamo Spertsyan <spertsiankamo@gmail.com>
2022-04-06 16:06:54 +0300

    Initial commit with Android and iOS modules. (#1)
    * Android module.
    
    * iOS module.
    
    * Renaming.
    
    * Update project.pbxproj
    
    * Update xcschememanagement.plist
    
    * Renamings.
    
    * Renamings.

---
+++ ios/sandwich/sandwich.h
@@ -0,0 +1,18 @@
+//
+//  sandwich.h
+//  sandwich
+//
+//  Created by Камо Сперцян on 06.04.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for sandwich.
+FOUNDATION_EXPORT double sandwichVersionNumber;
+
+//! Project version string for sandwich.
+FOUNDATION_EXPORT const unsigned char sandwichVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <sandwich/PublicHeader.h>
+
+
