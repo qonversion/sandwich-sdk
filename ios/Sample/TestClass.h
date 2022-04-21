2fbe18493197da8cdb24c3e2d195d01f83dde216
Surik <surik.sarkisyan@gmail.com>
2022-04-21 11:04:30 +0300

    Added sample app

---
+++ ios/Sample/TestClass.h
@@ -0,0 +1,17 @@
+//
+//  TestClass.h
+//  Sample
+//
+//  Created by Suren Sarkisyan on 20.04.2022.
+//  Copyright © 2022 Qonversion Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TestClass : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ios/Sample/TestSwiftClass.swift
@@ -0,0 +1,32 @@
+//
+//  TestSwiftClass.swift
+//  Sample
+//
+//  Created by Suren Sarkisyan on 20.04.2022.
+//  Copyright © 2022 Qonversion Inc. All rights reserved.
+//
+
+import Foundation
+import QonversionSandwich
+
+class TestSwiftClass {
+    
+    private lazy var sandwich = QonversionSandwich(qonversionEventListener: self)
+    
+    init() {
+        
+    }
+}
+
+// MARK: - QonversionEventListener
+
+extension TestSwiftClass: QonversionEventListener {
+    
+    func qonversionDidReceiveUpdatedPermissions(_ permissions: [String : Any]) {
+        
+    }
+    
+    func shouldPurchasePromoProduct(with productId: String) {
+        
+    }
+}
